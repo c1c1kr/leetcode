c79a8f17d9f6e7ce4c0a416f8f59ee9ecdb9dc0e
c1c1kr <c1c1kr2@gmail.com>
2023-04-27 00:24:36 +0900

    Time: 22 ms (41.24%), Space: 8.9 MB (46.20%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,28 @@
+#include <unordered_map> // hash_map
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> magazineLetters;   // make hash_map
+        
+        for(int i=0; i<magazine.length(); i++){
+            // can acess a string position by using .[pos]
+            char m = magazine[i];
+            magazineLetters[m]++;
+        }
+        for(int j=0; j<ransomNote.length(); j++){
+            
+            // can access a string position by using .[pos];
+            char r= ransomNote[j];
+            
+            int currentCount = magazineLetters[r];
+            
+            if(currentCount == 0 ){
+                return false;
+            }
+            magazineLetters[r]--;
+        }
+        
+        return true;
+    }
+};
